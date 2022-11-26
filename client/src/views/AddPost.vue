4bf25984dbe162fd153870ef621b9994b272c92d
JupickGosling <gizbrekht.y@mail.ru>
2022-11-26 12:26:28 +0300

    123

--- client/src/views/AddPost.vue
+++ client/src/views/AddPost.vue
@@ -1,5 +1,15 @@
 <template>
-    <h1>
-        Add Post Page
-    </h1>
+    <v-container>
+        <v-row no-gutters>
+            <v-col sm="10" class="mx-auto">
+                <v-card class="pa-5">
+                    <v-card-title>Add New Post</v-card-title>
+                    <v-divider></v-divider>
+                    <v-form ref="form" class="pa-5" enctype="multipart/form-data">
+                        <v-text-field label="Title" prepend-icon="mdo-note" ></v-text-field>
+                    </v-form>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
