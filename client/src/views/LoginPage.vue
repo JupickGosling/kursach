ee7f5e8a4b66ce62fc3fa4607c183126914d7099
JupickGosling <gizbrekht.y@mail.ru>
2022-11-30 23:52:16 +0300

    переделана страница авторизации

--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -1,54 +1,69 @@
 <template>
-  <v-form v-model="valid">
-    <v-container>
-      <v-row>
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="firstname"
-            :rules="nameRules"
-            :counter="10"
-            label="First name"
-            required
-          ></v-text-field>
-        </v-col>
+  <v-container>
+    <v-row no-gutters>
+        <v-col sm="10" class="mx-auto">
+            <v-card class="pa-5">
+                <v-card-title>Sign In</v-card-title>
+                <v-divider></v-divider>
+                <v-form ref="form" v-model="valid" lazy-validation>
+                  <v-text-field
+                    v-model="name"
+                    :counter="10"
+                    :rules="nameRules"
+                    label="Name"
+                    required
+                  ></v-text-field>
 
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="lastname"
-            :rules="nameRules"
-            :counter="10"
-            label="Last name"
-            required
-          ></v-text-field>
-        </v-col>
+                  <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="E-mail"
+                    required
+                  ></v-text-field>
 
-        <v-col
-          cols="12"
-          md="4"
-        >
-          <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            required
-          ></v-text-field>
+                  <v-select
+                    v-model="select"
+                    :items="items"
+                    :rules="[v => !!v || 'Item is required']"
+                    label="Item"
+                    required
+                  ></v-select>
+
+                  <v-checkbox
+                    v-model="checkbox"
+                    :rules="[v => !!v || 'You must agree to continue!']"
+                    label="Do you agree?"
+                    required
+                  ></v-checkbox>
+              
+                  <v-btn
+                    class="mr-4"
+                    @click="submit"
+                  >
+                    submit
+                  </v-btn>
+                  <v-btn @click="reset">
+                    clear
+                  </v-btn>
+                </v-form>
+            </v-card>
         </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
+    </v-row>
+</v-container>
 </template>
 <script>
   export default {
     data: () => ({
-      valid: false,
-      firstname: '',
-      lastname: '',
+      valid: true,
+      name: '',
+      select: null,
+      items: [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+      ],
+      checkbox: false,
       nameRules: [
         v => !!v || 'Name is required',
         v => v.length <= 10 || 'Name must be less than 10 characters',
@@ -56,8 +71,16 @@
       email: '',
       emailRules: [
         v => !!v || 'E-mail is required',
-        v => /.+@.+/.test(v) || 'E-mail must be valid',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
       ],
     }),
+    methods: {
+      submit () {
+        this.$v.$touch()
+      },
+      reset () {
+        this.$refs.form.reset()
+      },
+    },
   }
 </script>
